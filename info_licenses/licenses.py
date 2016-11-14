3cdcfebb809395cf4992b70c6b0ebfc372b23550
Joseph Roque <contact@josephroque.ca>
2016-11-13 22:33:06 -0500

    Change output format for licenses

--- info_licenses/licenses.py
+++ info_licenses/licenses.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import requests
 from collections import OrderedDict
 
@@ -14,7 +15,7 @@ if sources_json is None:
   exit()
 
 # Final source to print
-licenses = ''
+licenses = {}
 
 for source in sources_json:
   response = requests.get(sources_json[source])
@@ -23,11 +24,10 @@ for source in sources_json:
     print('Could not retrieve license for source: {0}'.format(source))
     exit()
   else:
-    licenses += '<b>{0}</b><br />'.format(source)
-    body = response.text.replace('"', '\\"')
-    body = body.replace('\n', '<br />')
-    licenses += body
+    # body = response.text.replace('"', r'\"')
+    body = re.sub(r'(?<!\n)\n(?!\n)|\n{3,}', '', response.text)
+    licenses[source] = [body]
 
 os.makedirs('./output/', exist_ok=True)
-with open('./output/licenses.txt', 'w+', encoding='utf8') as outfile:
-  outfile.write(licenses)
+with open('./output/licenses.json', 'w+', encoding='utf8') as outfile:
+  json.dump(licenses, outfile, sort_keys=True, ensure_ascii=False)
