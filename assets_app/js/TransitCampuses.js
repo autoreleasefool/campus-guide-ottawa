a2193b74f47098df8320bf83afd63a66aaa4ce63
Joseph Roque <contact@josephroque.ca>
2017-03-02 09:36:34 -0500

    Remove background color, add ID to transit

--- assets_app/js/TransitCampuses.js
+++ assets_app/js/TransitCampuses.js
@@ -24,28 +24,25 @@
 
 /* eslint-disable global-require */
 
-// Imports
-import * as Constants from 'Constants';
-
 module.exports = [
   {
-    background: Constants.Colors.transparentGarnet,
+    id: 'main',
     image: require('../images/placeholders/outer_placeholder.jpg'),
     name_en: 'Main Campus',
     name_fr: 'Campus principal',
   },
   {
-    background: Constants.Colors.transparentCharcoalGrey,
+    id: 'lees',
     image: require('../images/placeholders/outer_placeholder.jpg'),
     name: 'Lees',
   },
   {
-    background: Constants.Colors.transparentLightGrey,
+    id: 'rgn',
     image: require('../images/placeholders/outer_placeholder.jpg'),
     name: 'RGN',
   },
   {
-    background: Constants.Colors.transparentDarkGrey,
+    id: 'mba',
     image: require('../images/placeholders/outer_placeholder.jpg'),
     name: 'MBA',
   },
